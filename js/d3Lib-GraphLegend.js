c2e75f40c42c718e0963eae37fe435bf58d1ba23
Carlos Sebastian Ludueña <csluduena@gmail.com>
2024-04-11 19:56:15 -0300

    testeando

--- js/d3Lib-GraphLegend.js
+++ js/d3Lib-GraphLegend.js
@@ -1,1 +1,23 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const app = express();
 
+app.get('/pokeapi', async (req, res) => {
+    try {
+        const response = await fetch('https://pokeapi.co/api/v2/' + req.query.endpoint);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Something went wrong' });
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+fetch('http://127.0.0.1:5501/pokeapi?endpoint=pokemon')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
